b10e18caacd06d0150b7b4e612b07c00db86a0d6
Manikanta_Jami <Manikanta_Jami@EPINHYDW0463>
2017-02-06 13:05:40 +0530

    Add project files.

---
+++ sampleconsoleforgit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleconsoleforgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("welcome to git");
+
+
+        }
+    }
+}
